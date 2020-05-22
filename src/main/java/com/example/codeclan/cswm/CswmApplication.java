41e52d8ff47b24d1e196cf6e5c4b4acdcdfc355d
Stephen <st-jeeves@hotmail.co.uk>
2020-05-22 10:41:49 +0100

    first commit

---
+++ src/main/java/com/example/codeclan/cswm/CswmApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.cswm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CswmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CswmApplication.class, args);
+	}
+
+}
